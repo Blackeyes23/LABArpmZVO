0d554cf6e5f4643ee30399020d26613811e0cfcd
vanek <STUD@KIT-07-125>
2024-04-08 14:36:22 +0300

    я сделал библиотуку

--- Prilojenie1/Prilojenie1/Librarian.xaml.cs
+++ Prilojenie1/Prilojenie1/Librarian.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +13,17 @@ namespace Prilojenie1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Librarian : ContentPage
     {
+        protected internal ObservableCollection<book> Books { get; set; }
+
         public Librarian()
         {
             InitializeComponent();
+            Books = new ObservableCollection<book> 
+            {
+                 new book {Name="Финансист", Author="Теодор Драйзер", Genre="Роман"},
+                 new book {Name="1984", Author="Джордж Оруэлл", Genre="Роман"}
+            };
+            booklist.BindingContext = Books;
         }
     }
 }
---
+++ Prilojenie1/Prilojenie1/book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Prilojenie1
+{
+    public class book : INotifyPropertyChanged
+    {
+
+        private string name;
+        private string author;
+        private string genre;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (author != value)
+                {
+                    author = value;
+                    OnPropertyChanged("Author");
+                }
+            }
+        }
+        public string Genre
+        {
+            get { return genre; }
+            set
+            {
+                if (genre != value)
+                {
+                    genre = value;
+                    OnPropertyChanged("Genre");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
